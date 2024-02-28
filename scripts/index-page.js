e9c4c27775b949952f1f0ae9f5e6707bd822dc07
tnp_167 <tnp_167@outlook.com>
2024-02-28 20:52:52 +0000

    Adds pop-up windows to implement the reasons of delaying

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -14,6 +14,7 @@ const table = document.querySelector(".main__table");
 const lines = getlines()
     .then((line) => {
         renderLines(line);
+        const intervalId = setInterval(() =>  renderLines(line), 5000);
       })
     .catch((error) => {
         console.error("Error fetching comments:", error);
@@ -32,17 +33,49 @@ const displayLines = (line) =>{
     const status = document.createElement("p");
     status.classList.add("main__status");
     status.innerText = line.lineStatuses[0].statusSeverityDescription;
+
+    const popup = document.createElement("div");
+    popup.classList.add("main__popup");
+    status.appendChild(popup);
+
     line.lineStatuses[0].statusSeverity === 10 ?  status.classList.add("main__status--good") :  status.classList.add("main__status--delay")
+    
+    if(line.lineStatuses[0].statusSeverity !== 10){
+        status.addEventListener("mouseover", showPopup);
+        status.addEventListener("mouseout", hidePopup);
+    }
+    else{
+        status.removeEventListener("mouseover", showPopup);
+        status.removeEventListener("mouseover", hidePopup);
+    }
+
+    function showPopup(){
+        const hover = document.querySelector(".main__popup");
+        hover.classList.add("main__popup--active");
+        hover.innerText = line.lineStatuses[0].reason;
+    }
+
+    function hidePopup(){
+        const hover = document.querySelector(".main__popup");
+        hover.classList.remove("main__popup--active");
+    }
+
 
     container.appendChild(name);
     container.appendChild(status);
     
     table.appendChild(container);
+
 }
 
 const renderLines = (lines) => {
     console.log(lines);
     table.innerHTML = ""; 
+    const update = document.createElement("h3");
+    update.classList.add("main__update");
+    update.innerText = `Last updated: ${new Date()}`;
+    table.appendChild(update);
+
      lines.forEach(line => {
        displayLines(line);
      });
